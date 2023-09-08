5ca77e0be8e93c589ff825450d7e1f2b715f86f1
gambert.adrien <adrien62250@gmail.com>
2023-09-08 10:53:39 +0200

    creation of the front react

---
+++ front/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Home from  '././pages/homePage/Home';
+
+function App() {
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  );
+}
+
+export default App;
---
+++ front/src/pages/homePage/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Home () {
+    return (    
+        <header className="header">
+            <p>home</p>
+        </header>
+    );
+}
+
+export default Home;
