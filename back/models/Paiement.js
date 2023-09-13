bd237451a82b55eaf9c8d7ac2122d1d6526bef6f
Mayel Malebe <115797298+Mayel16@users.noreply.github.com>
2023-09-13 16:45:45 +0200

    Delete back directory

--- back/models/Paiement.js
+++
@@ -1,8 +0,0 @@
-class Paiement{constructor(Id_paiement, Id_client, Id_chambre){             
-    this.Id_paiement = Id_paiement;
-    this.Id_client = Id_client;
-    this.Id_chambre = Id_chambre;
-    }    
-}
-
-module.exports = {Paiement}
--- back/models/Prix.js
+++
@@ -1,9 +0,0 @@
-class Prix{constructor(prix, Id_chambre, saison){             
-    this.Id_prix = Id_prix;
-    this.prix = prix;
-    this.Id_chambre = Id_chambre;
-    this.saison = saison; 
-    }    
-}
-
-module.exports = {Prix}
