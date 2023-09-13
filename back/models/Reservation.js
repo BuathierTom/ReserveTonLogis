d21299b9ca44271176fa21e2f64f927eb7b5a89b
Mayel16 <corneilmalebe@gmail.com>
2023-09-13 16:48:57 +0200

    delete

--- back/models/Reservation.js
+++
@@ -1,9 +0,0 @@
-class reservation{constructor( Id_chambre, saison){             
-    this.Id_prix = Id_prix;
-    this.prix = prix;
-    this.Id_chambre = Id_chambre;
-    this.saison = saison; 
-    }    
-}
-
-module.exports = {reservation}
