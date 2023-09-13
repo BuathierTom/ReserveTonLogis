2173ad09a67cd0a5e90e08ea7bf813af8a4254f7
Mayel16 <corneilmalebe@gmail.com>
2023-09-13 16:52:10 +0200

    No sql

---
+++ Back/controllers/Client.js
@@ -0,0 +1,68 @@
+const crud = require('../services/crud.js')
+
+// Fonction de création d'un utilisateur
+async function createClient(req, res, next) {
+    try {
+        let test = await crud.insertOne('Client', req.body)
+        return res.send(test)
+        
+    } catch (e) {
+        console.log(`Erreur lors de l execution de la fonction createUser`);
+        console.log(e);
+        throw e;
+        
+    }
+}
+
+// Fonction de recherche d'un utilisateur avec son nom
+// A supprimer par la suite
+
+async function findClient(req, res, next) {
+    try {
+        let nom = req.params.nom;
+        let test = await crud.findOne('Client', {"nom" : nom})
+        return res.send(test)
+        
+    } catch (e) {
+        console.log(`Erreur lors de l execution de la fonction findUser`);
+        console.log(e);
+        throw e;
+    }
+}
+
+// Fonction de recherche de tous les utilisateurs
+// A supprimer par la suite
+
+async function findClientmany(req, res) {
+    try {
+        let nom = req.params.nom;
+        let test = await crud.find('Client')
+        return res.send(test)
+        
+    } catch (e) {
+        console.log(`Erreur lors de l execution de la fonction findUser`);
+        console.log(e);
+        throw e;
+    }
+}
+
+// Modifier les informations personnelles d'un utilisateur
+async function updateClient(req, res) {
+    try {
+        let id = req.params.id;
+        let test = await crud.updateOne('Client', {"nom" : id },{$set: { "nom": req.body.id_item}})
+        return res.send(test)
+    }
+    catch (e) {
+        console.log(`Erreur lors de l execution de la fonction updateItemStatus`);
+        console.log(e);
+        throw e;
+    }
+}
+
+module.exports = {
+    createClient,
+    findClient,
+    findClientmany,
+    updateClient
+};
