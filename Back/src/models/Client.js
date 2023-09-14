cfe013c6151154f99df55919332ede69c4987a92
BuathierTom <buathier_tom@orange.fr>
2023-09-14 17:27:03 +0200

    Ajustements

--- Back/src/models/Client.js
+++ Back/src/models/Client.js
@@ -1,12 +1,8 @@
-class Clients{constructor(id_client, nom, prenom, age, sexe, adresse, ville, codePostal, telephone, email, motDePasse){             
+class Clients{constructor(id_client, nom, prenom, adresse, telephone, email, motDePasse){             
     this.id_client = id_client;
     this.nom = nom;   
     this.prenom = prenom;
-    this.age = age;
-    this.sexe = sexe;
     this.adresse = adresse;
-    this.ville = ville;
-    this.codePostal = codePostal;
     this.telephone = telephone;
     this.email = email;
     this.motDePasse = motDePasse;
