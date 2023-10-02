1f8d019d26f26c5522f8eaa4d09e53106de65ea6
Mayel16 <corneilmalebe@gmail.com>
2023-10-02 14:31:05 +0200

    saison

--- Back/src/routes/saison.routes.js
+++ Back/src/routes/saison.routes.js
@@ -1,10 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const { findsaison, findsaisonMany, updateChambre, deleteChambre } = require ('../controllers/saison.controller.js');
+const { findsaison, findsaisonMany } = require ('../controllers/saison.controller.js');
 
 router.get("/getAll", findsaisonMany);
 router.get("/get/:id", findsaison);
-router.post("/update/:id", updateChambre);
-router.post("/delete/:id", deleteChambre);
-
 module.exports = router;
