5914162d50a4d7fce9b0844962247633bcd89aa3
BuathierTom <buathier_tom@orange.fr>
2023-10-06 09:13:30 +0200

    Fix bugs

--- Back/src/routes/reservations.routes.js
+++ Back/src/routes/reservations.routes.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { getAllReservations, createReservation } = require('../controllers/reservation.controller.js');
+const { getAllReservations, createReservation, getReservationById } = require('../controllers/reservation.controller.js');
 
 router.get('/getAll', getAllReservations);
+router.get('/get/:id', getReservationById);
 router.post('/create', createReservation);
 
 
