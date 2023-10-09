4d3ed5aef9d6cc65e8841cfcedc3cb78686142b5
Mayel16 <corneilmalebe@gmail.com>
2023-10-09 09:38:58 +0200

    modif

--- Back/src/controllers/client.controller.js
+++ Back/src/controllers/client.controller.js
@@ -54,7 +54,7 @@ const createClient = async (req, res, next) => {
         });
 
         const clientAdd = await newClient.save();
-        const emailContent = fs.readFileSync('./src/mail/mailText.mail.html', 'utf-8');
+        const emailContent = fs.readFileSync('./src/mail/createClient.mail.html', 'utf-8');
         //Envoi de l'e-mail au client
         const mailOptions = {
             from: process.env.MAIL_USER,
